f4cd8ca2cf598e69bb90c75a4406291ee174455e
Pedro Kondo <pedro.kondo@ideiasdinamicas.com>
2024-03-02 00:41:46 +0100

    test: não eliminar cache

--- src/domain/data/usecases/local-registar.spec.ts
+++ src/domain/data/usecases/local-registar.spec.ts
@@ -1,5 +1,19 @@
-describe('LocalRegistarCompras', ()=>{
-    test('Teste', ()=> {
-        expect(1).toBe(1)
+class LocalRegistarCompras {
+    constructor(private readonly cacheStore: CacheStore) { 
+    }
+}
+
+interface CacheStore {
+}
+
+class CacheStoreSpy implements CacheStore{
+    deleteCallsCount = 0
+}
+
+describe('LocalRegistarCompras', () => {
+    test('Should not delete cache on sut.init', () => {
+        const cacheStore = new CacheStoreSpy();
+        const sut = new LocalRegistarCompras(cacheStore)
+        expect(cacheStore.deleteCallsCount).toBe(0)
     })
 })
