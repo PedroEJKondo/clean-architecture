9beb03e68046bc9c23b30daa88b707e6fded20af
Pedro Kondo <pedro.kondo@ideiasdinamicas.com>
2024-03-02 01:01:11 +0100

    feat: remover cache apos salvar compras

--- src/domain/data/usecases/local-registar.spec.ts
+++ src/domain/data/usecases/local-registar.spec.ts
@@ -1,19 +1,35 @@
 class LocalRegistarCompras {
-    constructor(private readonly cacheStore: CacheStore) { 
+    constructor(private readonly cacheStore: CacheStore) {
+    }
+
+    async registar(): Promise<void> {
+        this.cacheStore.delete()
     }
 }
 
 interface CacheStore {
+    delete: () => void
 }
 
-class CacheStoreSpy implements CacheStore{
+class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0
+    delete(): void {
+        this.deleteCallsCount++
+    }
 }
 
 describe('LocalRegistarCompras', () => {
+
     test('Should not delete cache on sut.init', () => {
         const cacheStore = new CacheStoreSpy();
-        const sut = new LocalRegistarCompras(cacheStore)
+        new LocalRegistarCompras(cacheStore)
         expect(cacheStore.deleteCallsCount).toBe(0)
+    })
+
+    test('Should delete old cache on sut.init', async () => {
+        const cacheStore = new CacheStoreSpy();
+        const sut = new LocalRegistarCompras(cacheStore)
+        await sut.registar()
+        expect(cacheStore.deleteCallsCount).toBe(1)
     })
 })
