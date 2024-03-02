dbec276a245cb6e1ecbeed7b23af9b844a1527fe
Pedro Kondo <pedro.kondo@ideiasdinamicas.com>
2024-03-02 03:37:09 +0100

    feat: will not insert new cache if delete fails

--- src/data/usecases/registar-compras/local-registar-compras.spec.ts
+++ src/data/usecases/registar-compras/local-registar-compras.spec.ts
@@ -3,11 +3,16 @@ import { LocalRegistarCompras } from '@/data/usecases'
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0
+    insertCallsCount = 0
     key: string = ""
 
     delete(key: string): void {
         this.deleteCallsCount++
         this.key = key
+    }
+
+    insert(input: string): void {
+        this.insertCallsCount = 0
     }
 }
 
@@ -38,6 +43,14 @@ describe('LocalRegistarCompras', () => {
         await sut.registar()
         expect(cacheStore.deleteCallsCount).toBe(1)
         expect(cacheStore.key).toBe('compras')
+    })
+
+    test('Should not insert new Cache if delete fails', async () => {
+        const { sut, cacheStore } = makeSut()
+        jest.spyOn(cacheStore, 'delete').mockImplementationOnce(()=>{throw new Error()})
+        const promise = sut.registar()
+        expect(cacheStore.insertCallsCount).toBe(0)
+        expect(promise).rejects.toThrow()
     })
 
 })
