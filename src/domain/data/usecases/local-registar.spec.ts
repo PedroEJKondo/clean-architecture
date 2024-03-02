9a9e6bfa075835062f166d4ce7c1be9b03c10e80
Pedro Kondo <pedro.kondo@ideiasdinamicas.com>
2024-03-02 01:09:38 +0100

    feat: using makesut

--- src/domain/data/usecases/local-registar.spec.ts
+++ src/domain/data/usecases/local-registar.spec.ts
@@ -1,6 +1,5 @@
 class LocalRegistarCompras {
-    constructor(private readonly cacheStore: CacheStore) {
-    }
+    constructor(private readonly cacheStore: CacheStore) { }
 
     async registar(): Promise<void> {
         this.cacheStore.delete()
@@ -18,18 +17,32 @@ class CacheStoreSpy implements CacheStore {
     }
 }
 
+type SutTypes = {
+    sut: LocalRegistarCompras
+    cacheStore: CacheStoreSpy 
+}
+
+const makeSut = (): SutTypes => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalRegistarCompras(cacheStore)
+
+    return {
+        sut,
+        cacheStore
+    }
+}
+
 describe('LocalRegistarCompras', () => {
 
     test('Should not delete cache on sut.init', () => {
-        const cacheStore = new CacheStoreSpy();
-        new LocalRegistarCompras(cacheStore)
+        const { cacheStore } = makeSut()
         expect(cacheStore.deleteCallsCount).toBe(0)
     })
 
     test('Should delete old cache on sut.init', async () => {
-        const cacheStore = new CacheStoreSpy();
-        const sut = new LocalRegistarCompras(cacheStore)
+        const { sut, cacheStore } = makeSut()
         await sut.registar()
         expect(cacheStore.deleteCallsCount).toBe(1)
     })
+
 })
